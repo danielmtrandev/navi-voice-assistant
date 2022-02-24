2ee6b18659a038f184261187d5d7f6918cca551a
Daniel <danielmtran.dev@gmail.com>
2022-02-24 13:52:26 -0500

    navi

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# navi-voice-assistant
-This is the first version of my voice-controlled application called Navi. Utilizing Alan AI for it's voice control features, Navi's main functionality is pulling news report through the News API while also being able to provide general information regarding topics such as the weather, cryptocurrency etc.
+### APPLICATION: Navi Voice Assistant
+
+Navi is a voice-controlled application that allows users to retrieve useful information ranging from the latest news articles, cryptocurrency, the weather, etc.
+
+<img src=""></img>
+
+Link to live site: [https://navi-voice-assistant.herokuapp.com/]
+
+### How It's Made:
+
+Tech used: React.js, Material UI, News API, Alan API, Vanilla Javascript
+
+### Optimizations:
+
+Currently trying to add more functionality on top of what I had learned so far.
+
+
+### Lessons Learned:
+
+I had the most fun making this app due to the sheer amount of utility that Alan API offered.
+
+
+## Installation
+
+1. Clone repo
+2. run `npm install`
+
+## Usage
+
+1. run `node server.js`
+2. Navigate to `localhost:3000`
+
+
---
+++ src/App.js
@@ -0,0 +1,60 @@
+// overall config to run the app
+
+// useEffect is a function that happens once the code starts / when the app mounts
+import React, { useState, useEffect } from 'react';
+// adding alan into app
+import alanBtn from '@alan-ai/alan-sdk-web';
+
+// required to mitigate certain voice to text issues that occur when requiring numbers
+// User: Open article 4 
+// Alan: 
+import wordsToNumbers from 'words-to-numbers';
+
+import NewsCards from './components/NewsCards/NewsCards';
+import useStyles from './styles.js';
+
+const alanKey = '1f4a2871d90c066bd662c1fc37feb3802e956eca572e1d8b807a3e2338fdd0dc/stage';
+
+const App = () => {
+    const [newsArticles, setNewsArticles] = useState([]);
+    const [activeArticle, setActiveArticle] = useState(-1);
+    const classes = useStyles();
+
+    useEffect(() => {
+        // generates an Alan button on the browser
+        alanBtn({
+            key: alanKey,
+            // how Alan can listen to specific commands
+            onCommand: ({ command, articles, number }) => {
+                if(command === 'newHeadlines'){
+                   setNewsArticles(articles);
+                   setActiveArticle(-1);
+                } else if(command === 'highlight'){
+                    setActiveArticle((prevActiveArticle) => prevActiveArticle + 1); 
+                } else if(command === 'open') {
+                    const parsedNumber = number.length > 2 ? wordsToNumbers(number, { fuzzy: true }) : number;
+                    const article = articles[parsedNumber - 1];
+                    if(parsedNumber > 20){
+                        alanBtn().playText('Please try that again.')
+                    } else if(article) {
+                        window.open(article.url, '_blank');
+                        alanBtn().playText('Opening ... ');
+                    }
+                }
+            }
+        })
+    }, [])
+        
+    return (
+        <div>
+            <div className={classes.logoContainer}>
+                <img src="cover.png" className={classes.alanLogo} alt="alan logo" />
+            </div>
+
+            <NewsCards articles={newsArticles} activeArticle={activeArticle}/>
+        </div>
+    );
+}
+
+export default App;
+
